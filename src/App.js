f2cc68d95dab3dab39ddd97a60bbd11b94b91acf
sowjanyam27 <sowjanya.m94@gmail.com>
2020-06-30 15:16:27 +0200

    Added Login and SignUp form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
 import "./App.css";
 
+import { Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Loading from "./components/Loading";
+import MessageBox from "./components/MessageBox";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
+import { useDispatch, useSelector } from "react-redux";
+import { selectAppLoading } from "./store/appState/selectors";
+import { getUserWithStoredToken } from "./store/user/actions";
+import { Jumbotron } from "react-bootstrap";
+
 function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectAppLoading);
+
+  useEffect(() => {
+    dispatch(getUserWithStoredToken());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <h1>Welcome to travel wallet!!!</h1>
+      <Navigation />
+      <MessageBox />
+      {isLoading ? <Loading /> : null}
+      <Switch>
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
   );
 }
