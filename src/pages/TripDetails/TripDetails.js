0840bdc94e082cc28255968c41ec839f02ee265c
sowjanyam27 <sowjanya.m94@gmail.com>
2020-07-08 10:23:26 +0200

    Added budget and money spent details and logos

--- src/pages/TripDetails/TripDetails.js
+++ src/pages/TripDetails/TripDetails.js
@@ -9,29 +9,64 @@ import { selectToken, selectUser } from "../../store/user/selectors";
 import { selectExpenses } from "../../store/AddExpense/selector";
 import Container from "react-bootstrap/Container";
 import { FaTrash } from "react-icons/fa";
+import {
+  Fastfood,
+  Flight,
+  ShoppingCart,
+  House,
+  Notes,
+  Delete,
+} from "@material-ui/icons";
 
 import "./Tripdetails.css";
 import { fetchAllUsersforTrip } from "../../store/Homepage/actions";
-// get our fontawesome imports
+import { selectUsersofTrips } from "../../store/Homepage/selector";
+import { selectNewTrip } from "../../store/Addtrip/selectors";
 
 export default function TripDetails() {
   const { id } = useParams();
   const dispatch = useDispatch();
   const token = useSelector(selectToken);
   const { expenses } = useSelector(selectExpenses);
+  const [total, setTotal] = useState(0);
+  const [budget, setBudget] = useState(0);
   const history = useHistory();
-
+  const { trip } = useSelector(selectNewTrip);
+  const tripUsers = useSelector(selectUsersofTrips);
+  console.log("tripUsers:", tripUsers);
   useEffect(() => {
     dispatch(fetchAllExpensesofTrip(id, token));
     dispatch(fetchAllUsersforTrip(id, token));
   }, [id]);
 
   const deleteExpense = (id) => {
-    console.log("id:", id);
     dispatch(deleteExpenseDetails(id));
   };
 
+  useEffect(() => {
+    console.log("expenses inside useEffect:", expenses);
+
+    const amount = expenses.reduce(function (acc, obj) {
+      return acc + obj.amount;
+    }, 0);
+    setTotal(amount);
+    if (expenses.length) {
+      console.log("trip:", trip);
+      //setBudget(expenses[0].trip.budget);
+    }
+  }, [expenses]);
+
+  useEffect(() => {
+    console.log("tripUsers inside useEffect:", tripUsers);
+    if (tripUsers.length !== 0) {
+      setBudget(tripUsers[0].trip.budget);
+    }
+  }, [tripUsers]);
+
   console.log("expenses:", expenses);
+  console.log("total:", total);
+  console.log("budget:", budget);
+
   return (
     <div className="expense">
       <Container className="cont">
@@ -52,19 +87,35 @@ export default function TripDetails() {
           </div>
           <div className="card-body">
             <h5 className="card-title">Trip Expenses</h5>
+            <p className="mx-3 text-muted">
+              {" "}
+              Budget: € ${budget}{" "}
+              <span className="mx-3">Amount Spent: € ${total}</span>
+            </p>
             <div className="mt-5">
               <div className="card" style={{ borderStyle: "none" }}>
                 <ul className="list-group list-group-flush">
                   {expenses.map((expense, i) => {
                     return (
                       <div key={i} className="list-item">
+                        {expense.expensetypeId === 1 ? (
+                          <Fastfood />
+                        ) : expense.expensetypeId === 2 ? (
+                          <Flight />
+                        ) : expense.expensetypeId === 3 ? (
+                          <House />
+                        ) : expense.expensetypeId === 4 ? (
+                          <ShoppingCart />
+                        ) : (
+                          <Notes />
+                        )}
                         <li className="list-inline-item">{expense.title}</li>
-                        <li className="list-inline-item">{expense.amount}</li>
+                        <li className="list-inline-item">€ {expense.amount}</li>
                         <button
                           className="list-buttons"
                           onClick={() => deleteExpense(expense.id)}
                         >
-                          <FaTrash />
+                          <Delete />
                         </button>
                       </div>
                     );
