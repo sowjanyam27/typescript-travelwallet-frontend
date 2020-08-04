6d2efe04778d437552f2f4845d4be936b0cc18ea
sowjanyam27 <sowjanya.m94@gmail.com>
2020-08-04 17:53:23 +0200

    Reverted to heruko server

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
-export const apiUrl = "http://localhost:4000";
-// process.env.API_URL || "https://travel-wallet-portfolio.herokuapp.com";
+export const apiUrl = //"http://localhost:4000";
+  process.env.API_URL || "https://travel-wallet-portfolio.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const staticUrl = "http://localhost:4000/static";
 // process.env.API_URL || "https://travel-wallet-portfolio.herokuapp.com/static";
